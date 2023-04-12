eba8f8ec9b03648c479c9de82fa4346ea883b433
CatSci <atul.yadav@catsci.com>
2023-04-12 10:53:15 +0100

    eid

--- src/eln.py
+++ src/eln.py
@@ -11,11 +11,15 @@ from urllib.parse  import urlparse, parse_qs
 import requests
 
 # url = "https://cas2inchi.streamlit.app/?__eid=grid%3A13cce184-65a8-4153-9620-5aafbc9c2511"
-url = requests.get('https://cas2inchi.streamlit.app/').url
-parse_url = urlparse(url)
-eid = parse_qs(parse_url.query)['__eid'][0]
+try:
+    url = requests.get('https://cas2inchi.streamlit.app/').url
+    parse_url = urlparse(url)
+    eid = parse_qs(parse_url.query)['__eid'][0]
+    st.write(eid)
+except Exception as e:
+    pass
 
-st.write(eid)
+
 
 # load_dotenv(".env")
 # API_KEY = os.getenv("API_KEY")
