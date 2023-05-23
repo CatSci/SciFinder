dd9151003016d8bcc9d0d1f7accbf342d2e60053
CatSci <atul.yadav@catsci.com>
2023-05-23 11:23:12 +0100

    production eln

--- src/constant/eln_constant.py
+++ src/constant/eln_constant.py
@@ -1,1 +1,3 @@
-API_BASE_URL = 'https://catsci-sandbox.signalsnotebook.perkinelmercloud.eu/api/rest/v1.0/adt/'
+# API_BASE_URL = 'https://catsci-sandbox.signalsnotebook.perkinelmercloud.eu/api/rest/v1.0/adt/'
+
+API_BASE_URL = 'https://catsci.signalsnotebook.perkinelmercloud.eu/api/rest/v1.0/adt/'
